4d422cb71244031a310da2ffe71d47cea64cd42d
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-24 14:33:48 +0100

    added all the plugins, files and necessary files

---
+++ my_portfolio/lib/injection.dart
@@ -0,0 +1,14 @@
+// import 'package:Prince/domain/contact_viewmodel.dart';
+// import 'package:Prince/services/implementation/impl_contact_service.dart';
+// import 'package:locator/locator.dart';
+// // import 'package:yelwinoo/domain/contact_viewmodel.dart';
+// // import 'package:yelwinoo/services/implementation/impl_contact_service.dart';
+
+// const locator = Locator();
+
+// class Injection {
+//   static void setUp() {
+//     locator.put(ImplContactService());
+//     locator.put(ContactViewModel(service: locator.get<ImplContactService>()));
+//   }
+// }
---
+++ my_portfolio/lib/services/abstract/contact_service.dart
@@ -0,0 +1,5 @@
+// import 'package:Prince/data/model/contact.dart';
+
+// abstract class ContactService {
+//   Future<void> sendMessage(Contact contact);
+// }
