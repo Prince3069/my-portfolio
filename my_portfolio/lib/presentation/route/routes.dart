9defce5d381c579bd6e8d1b36d386acf7f6a7de3
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-24 16:10:11 +0100

    added routes

--- my_portfolio/lib/presentation/route/routes.dart
+++ my_portfolio/lib/presentation/route/routes.dart
@@ -1,83 +1,83 @@
-// // ignore_for_file: unused_import
+// ignore_for_file: unused_import
 
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:Prince/data/model/showcase_project.dart';
-// import 'package:Prince/presentation/configs/configs.dart';
-// import 'package:Prince/presentation/utils/extensions/extensions.dart';
-// import 'package:Prince/presentation/views/about/about_view.dart';
-// import 'package:Prince/presentation/views/certificates/certificates_view.dart';
-// import 'package:Prince/presentation/views/contact_me/contact_me_view.dart';
-// import 'package:Prince/presentation/views/experience/experience_view.dart';
-// import 'package:Prince/presentation/views/home/home_page.dart';
-// import 'package:Prince/presentation/views/project_details/project_details_view.dart';
-// import 'package:Prince/presentation/views/projects/projects_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:Prince/data/model/showcase_project.dart';
+import 'package:Prince/presentation/configs/configs.dart';
+import 'package:Prince/presentation/utils/extensions/extensions.dart';
+import 'package:Prince/presentation/views/about/about_view.dart';
+import 'package:Prince/presentation/views/certificates/certificates_view.dart';
+import 'package:Prince/presentation/views/contact_me/contact_me_view.dart';
+import 'package:Prince/presentation/views/experience/experience_view.dart';
+import 'package:Prince/presentation/views/home/home_page.dart';
+import 'package:Prince/presentation/views/project_details/project_details_view.dart';
+import 'package:Prince/presentation/views/projects/projects_view.dart';
 
-// import 'route_transitions.dart';
+import 'route_transitions.dart';
 
-// class RouteGen {
-//   static Route<dynamic> generateRoute(RouteSettings settings) {
-//     final arguments = settings.arguments;
-//     switch (settings.name) {
-//       case Routes.home:
-//         return _buildRoute(const HomePage(), settings: settings);
-//       case Routes.projects:
-//         return _buildRoute(const ProjectsView(), settings: settings);
-//       case Routes.projectDetails:
-//         return _buildRoute(
-//           ProjectDetailsView(
-//             project: arguments as ShowcaseProject,
-//           ),
-//           settings: RouteSettings(
-//             name: '${settings.name}/${arguments.title}',
-//           ),
-//         );
-//       case Routes.experience:
-//         return _buildRoute(const ExperienceView(), settings: settings);
-//       case Routes.certificates:
-//         return _buildRoute(const CertificatesView(), settings: settings);
-//       case Routes.about:
-//         return _buildRoute(const AboutView(), settings: settings);
-//       case Routes.contact:
-//         return _buildRoute(const ContactMeView(), settings: settings);
-//       default:
-//         return _buildRoute(const ErrorView(), settings: settings);
-//     }
-//   }
+class RouteGen {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final arguments = settings.arguments;
+    switch (settings.name) {
+      case Routes.home:
+        return _buildRoute(const HomePage(), settings: settings);
+      case Routes.projects:
+        return _buildRoute(const ProjectsView(), settings: settings);
+      case Routes.projectDetails:
+        return _buildRoute(
+          ProjectDetailsView(
+            project: arguments as ShowcaseProject,
+          ),
+          settings: RouteSettings(
+            name: '${settings.name}/${arguments.title}',
+          ),
+        );
+      case Routes.experience:
+        return _buildRoute(const ExperienceView(), settings: settings);
+      case Routes.certificates:
+        return _buildRoute(const CertificatesView(), settings: settings);
+      case Routes.about:
+        return _buildRoute(const AboutView(), settings: settings);
+      case Routes.contact:
+        return _buildRoute(const ContactMeView(), settings: settings);
+      default:
+        return _buildRoute(const ErrorView(), settings: settings);
+    }
+  }
 
-//   static PageRouteBuilder _buildRoute(Widget child, {RouteSettings? settings}) {
-//     return SlideRouteTransition(
-//       settings: settings,
-//       enterWidget: child,
-//     );
-//   }
-// }
+  static PageRouteBuilder _buildRoute(Widget child, {RouteSettings? settings}) {
+    return SlideRouteTransition(
+      settings: settings,
+      enterWidget: child,
+    );
+  }
+}
 
-// class Routes {
-//   static const home = "/";
-//   static const certificates = "/certificates";
-//   static const projects = "/projects";
-//   static const experience = "/experience";
-//   static const projectDetails = "/project_details";
-//   static const about = "/about";
-//   static const contact = "/contact_me";
-// }
+class Routes {
+  static const home = "/";
+  static const certificates = "/certificates";
+  static const projects = "/projects";
+  static const experience = "/experience";
+  static const projectDetails = "/project_details";
+  static const about = "/about";
+  static const contact = "/contact_me";
+}
 
-// class ErrorView extends StatelessWidget {
-//   const ErrorView({super.key});
+class ErrorView extends StatelessWidget {
+  const ErrorView({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Error Page'),
-//       ),
-//       body: Center(
-//         child: Text(
-//           "404 - Page Not Found!",
-//           style: context.titleLarge,
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Error Page'),
+      ),
+      body: Center(
+        child: Text(
+          "404 - Page Not Found!",
+          style: context.titleLarge,
+        ),
+      ),
+    );
+  }
+}
