afe1ffbcedf9b0d32244fdc8b9b329ac3d812678
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-24 14:37:05 +0100

    added main.dart file

--- my_portfolio/lib/main.dart
+++ my_portfolio/lib/main.dart
@@ -1,43 +1,43 @@
-// import 'package:Prince/injection.dart';
-// import 'package:Prince/presentation/utils/extensions/extensions.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:layout/layout.dart';
-// import 'package:url_strategy/url_strategy.dart';
-// import 'firebase_options.dart';
-// import 'presentation/route/routes.dart';
-// import 'presentation/utils/custom_scroll_behaviour.dart';
+import 'package:Prince/injection.dart';
+import 'package:Prince/presentation/utils/extensions/extensions.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:layout/layout.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'firebase_options.dart';
+import 'presentation/route/routes.dart';
+import 'presentation/utils/custom_scroll_behaviour.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   await SystemChrome.setPreferredOrientations([
-//     DeviceOrientation.portraitUp,
-//   ]);
-//   Injection.setUp();
-//   setPathUrlStrategy();
-//   runApp(const TucoYe());
-// }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  Injection.setUp();
+  setPathUrlStrategy();
+  runApp(const TucoYe());
+}
 
-// class TucoYe extends StatelessWidget {
-//   const TucoYe({super.key});
+class TucoYe extends StatelessWidget {
+  const TucoYe({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Layout(
-//       child: MaterialApp(
-//         title: 'Prince',
-//         scrollBehavior: AppScrollBehavior(),
-//         debugShowCheckedModeBanner: false,
-//         theme: context.theme(),
-//         initialRoute: initialRoute,
-//         onGenerateRoute: RouteGen.generateRoute,
-//       ),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Layout(
+      child: MaterialApp(
+        title: 'Prince',
+        scrollBehavior: AppScrollBehavior(),
+        debugShowCheckedModeBanner: false,
+        theme: context.theme(),
+        initialRoute: initialRoute,
+        onGenerateRoute: RouteGen.generateRoute,
+      ),
+    );
+  }
 
-//   String get initialRoute => Routes.home;
-// }
+  String get initialRoute => Routes.home;
+}
