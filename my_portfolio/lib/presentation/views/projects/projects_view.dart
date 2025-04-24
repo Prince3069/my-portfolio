ec8e630d18c44e82a5ecc6176a749fdd302a6533
Prince3069 <Princenwanozie6666@gmail.com>
2025-04-24 15:42:28 +0100

    added project view file

--- my_portfolio/lib/presentation/views/projects/projects_view.dart
+++ my_portfolio/lib/presentation/views/projects/projects_view.dart
@@ -1,28 +1,28 @@
-// import 'package:flutter/material.dart';
-// import 'package:Prince/presentation/utils/extensions/extensions.dart';
-// import 'package:Prince/presentation/views/wrapper.dart';
+import 'package:flutter/material.dart';
+import 'package:Prince/presentation/utils/extensions/extensions.dart';
+import 'package:Prince/presentation/views/wrapper.dart';
 
-// import 'subviews/project_list_page.dart';
-// import 'subviews/project_title_page.dart';
+import 'subviews/project_list_page.dart';
+import 'subviews/project_title_page.dart';
 
-// class ProjectsView extends StatefulWidget {
-//   const ProjectsView({super.key});
+class ProjectsView extends StatefulWidget {
+  const ProjectsView({super.key});
 
-//   @override
-//   State<ProjectsView> createState() => _ProjectsViewState();
-// }
+  @override
+  State<ProjectsView> createState() => _ProjectsViewState();
+}
 
-// class _ProjectsViewState extends State<ProjectsView> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Wrapper(
-//       page: const <Widget>[
-//         ProjectTitlePage(),
-//         ProjectListPage(),
-//       ].addListView(
-//         physics: const ClampingScrollPhysics(),
-//         scrollDirection: Axis.vertical,
-//       ),
-//     );
-//   }
-// }
+class _ProjectsViewState extends State<ProjectsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Wrapper(
+      page: const <Widget>[
+        ProjectTitlePage(),
+        ProjectListPage(),
+      ].addListView(
+        physics: const ClampingScrollPhysics(),
+        scrollDirection: Axis.vertical,
+      ),
+    );
+  }
+}
